ef1b62e80c6bb860c9f3874d1b7f1c62c6fd882c
chiheng <chiheng357@gmail.com>
2024-12-05 08:59:14 +0700

    Finish step 2, TP4:   **install vue-router   **create HomeView.vue   **Move App.vue to HomeView.vue   **Route HomeView from App.vue using router link

--- gic2024/e-commerce/src/router/index.js
+++ gic2024/e-commerce/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
+const routes = [
+  {
+    path: "/Homeview",
+    component: HomeView,
+  }
+]
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
